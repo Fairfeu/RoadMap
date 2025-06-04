fac4427d6ba6d56b4acfbd4e09e9f7f722c1c5fb
Oleg <fnytseu@gmail.com>
2025-06-04 20:52:53 +0300

    fix error page

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,26 +3,29 @@ import Layout from "./components/Layout";
 import ROUTES from "./links";
 import pagesMap from "./pagesMap";
 import createLazyRoute from "./lazyRout";
+import ErrorPage from "./topics/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: Object.values(ROUTES).map((path) => {
-      const Component = createLazyRoute(pagesMap[path]);
+    children: [
+      ...Object.values(ROUTES)
+        .filter((path) => typeof path === "string" && path.startsWith("/"))
+        .map((path) => {
+          const Component = createLazyRoute(pagesMap[path]);
+          return {
+            index: path === "/",
+            path: path === "/" ? undefined : path.replace("/", ""),
+            element: <Component />,
+          };
+        }),
 
-      if (path === "/") {
-        return {
-          index: true,
-          element: <Component />,
-        };
-      }
-
-      return {
-        path: path.replace("/", ""),
-        element: <Component />,
-      };
-    }),
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
   },
 ]);
 
